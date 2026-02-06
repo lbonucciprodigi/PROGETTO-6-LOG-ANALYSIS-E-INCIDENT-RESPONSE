a3510a6e25b0e01a3a7e0a119d9fd80af9448973
FilippoErmini <filippoermini@filippoerminiVM.mshome.net>
2026-02-06 13:57:33 +0100

    Add analyzer

--- analyze-logs.sh
+++ analyze-logs.sh
@@ -1,3 +1,24 @@
-#! /bin/bash
+#!/bin/bash
 
+#Checking if IP trying to BRUTE FORCE via SSH
 
+THRESHOLD = 5
+
+grep "Failed password" /var/log/auth.log | awk '{print $9}' | sort | uniq -c |
+while read count ip; do
+	if ["$count" -gt "$THRESHOLD"]; then
+		echo "ALERT: IP $ip trying to BRUTE FORCE $count times"
+	fi
+done	
+
+#List of TOP IP attackers
+
+grep "Failed password" /var/log/auth.log | awk '{print $9}' | sort | uniq -c > top-attacker.txt
+
+#List of sudo COMMAND
+
+grep "sudo:" /var/log/auth.log | grep "COMMAND"
+
+#Analyze HTTP errors
+
+awk '$9 ~ /^[45]/ {print $1, $7, $9}' /var/log/apache2/access.log
